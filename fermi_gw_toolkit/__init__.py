eef8534316ea2df50f9cbfcd5de9f2dddf2ca75e
ndilalla <niccolo.dilalla@pi.infn.it>
2016-11-28 10:42:21 +0100

    FERMI_GW_OUTPUT and FERMI_GW_DATA defined.

--- fermi_gw_toolkit/__init__.py
+++ fermi_gw_toolkit/__init__.py
@@ -1,1 +1,20 @@
 __author__ = 'giacomov'
+
+import os
+
+PACKAGE_NAME = 'fermi_gw_toolkit'
+
+FERMI_GW_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    FERMI_GW_DATA = os.environ['FERMI_GW_DATA']
+    print 'Fermi data directory set to: %s' % FERMI_GW_DATA
+except:
+    print 'You have to set FERMI_GW_DATA before proceding with the analysis.'
+
+
+try:
+    FERMI_GW_OUTPUT = os.environ['FERMI_GW_OUTPUT']
+except:
+    FERMI_GW_OUTPUT = FERMI_GW_ROOT
+print 'Output directory set to: %s ' % FERMI_GW_OUTPUT
