f9340f7115b9b7bd824f9f6eb1b1f84a7b469c1e
ndilalla <niccolo.dilalla@pi.infn.it>
2016-12-19 14:53:31 +0100

    Refactoring of the code.

--- fermi_gw_toolkit/fill_maps.py
+++ fermi_gw_toolkit/fill_maps.py
@@ -7,26 +7,26 @@ import numpy as np
 import healpy as hp
 import warnings
 
-from check_file_exists import check_file_exists
+from fermi_gw_toolkit.check_file_exists import check_file_exists
+from fermi_gw_toolkit.sky_to_healpix_id import sky_to_healpix_id
 
-from sky_to_healpix_id import sky_to_healpix_id
+desc = '''Fill the input HEALPIX map with the values taken from the input text file'''
 
+formatter = argparse.ArgumentDefaultsHelpFormatter
+parser = argparse.ArgumentParser(description=desc, formatter_class=formatter)
 
-if __name__=="__main__":
+parser.add_argument('--in_map', help='Input HEALPIX map',
+                    type=check_file_exists, required=True)
+parser.add_argument('--text_file', type=check_file_exists,required=True,
+                    help='Input text file (results from the doTimeResolvedLike script of gtburst)')
+parser.add_argument('--out_uls_map', type=str, required=True,
+                    help='Name for the output upper limits map')
+parser.add_argument('--out_ts_map', help='Name for the output TS map',
+                    type=str, required=True)
 
-    desc = '''Fill the input HEALPIX map with the values taken from the input text file'''
-
-    parser = argparse.ArgumentParser(description=desc)
-
-    parser.add_argument('--in_map',help='Input HEALPIX map', type=check_file_exists, required=True)
-    parser.add_argument('--text_file',help='Input text file (results from the doTimeResolvedLike script of gtburst)',
-                        type=check_file_exists,required=True)
-    parser.add_argument('--out_uls_map',help='Name for the output upper limits map', type=str, required=True)
-    parser.add_argument('--out_ts_map',help='Name for the output TS map', type=str, required=True)
-
-    args = parser.parse_args()
-
-    data = np.recfromtxt(args.text_file, names=True)
+def fill_maps(**kwargs):
+    
+    data = np.recfromtxt(kwargs['text_file'], names=True)
 
     # Check that the fluxes are upper limits
 
@@ -47,7 +47,7 @@ if __name__=="__main__":
 
     # Get the NSIDE from the input healpix map
 
-    hpx_orig, header = hp.read_map(args.in_map, h=True, verbose=False)
+    hpx_orig, header = hp.read_map(kwargs['in_map'], h=True, verbose=False)
 
     nside = hp.npix2nside(hpx_orig.shape[0])
 
@@ -65,7 +65,7 @@ if __name__=="__main__":
 
     # Write the upper limit map
 
-    hp.write_map(args.out_uls_map, upper_limits_map, coord='C')
+    hp.write_map(kwargs['out_uls_map'], upper_limits_map, coord='C')
 
     # Now the map of TS
 
@@ -83,4 +83,8 @@ if __name__=="__main__":
 
     # Write the upper limit map
 
-    hp.write_map(args.out_ts_map, ts_map, coord='C')
+    hp.write_map(kwargs['out_ts_map'], ts_map, coord='C')
+
+if __name__=="__main__":
+    args = parser.parse_args()
+    fill_maps(**args.__dict__)
