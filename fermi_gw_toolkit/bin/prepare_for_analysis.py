e29e9e8b6eb7dedc25c82ccc780e85cd1891300d
ndilalla <niccolo.dilalla@pi.infn.it>
2017-02-19 19:44:47 +0100

    Added adaptive_time.

--- fermi_gw_toolkit/bin/prepare_for_analysis.py
+++ fermi_gw_toolkit/bin/prepare_for_analysis.py
@@ -26,6 +26,7 @@ try:
     FT1 = check_file_exists(config.FT1)
     FT2 = check_file_exists(config.FT2)
 except:
+    print "No FT1 or FT2 found."
     download = True
 
 TSTART = config.TSTART
@@ -36,6 +37,7 @@ IRF = config.IRF
 ZMAX = config.ZMAX
 THETAMAX = config.THETAMAX
 
+OUTTIME = config.OUTTIME
 OUTLIST = config.OUTLIST
 OUTMAP = config.OUTMAP
 OUTCOV = config.OUTCOV
@@ -47,18 +49,24 @@ PIPELINE = gwPipeline()
 
 def prepare_for_analysis():
     if download:
-        FT1, FT2 = PIPELINE.download_LAT_data(TRIGGERNAME, emin=EMIN,
+        _FT1, _FT2 = PIPELINE.download_LAT_data(TRIGGERNAME, emin=EMIN,
                             emax=EMAX, outdir=DATA_PATH, irf=IRF,
                             tstart=TRIGGERTIME-1000, tstop=TRIGGERTIME+10000)
+    else:
+        _FT1, _FT2 = FT1, FT2
     
-    ft1, rsp, ft2, pha = PIPELINE.rawdata2package(ft1=FT1, ft2=FT2,
+    ft1, rsp, ft2, pha = PIPELINE.rawdata2package(ft1=_FT1, ft2=_FT2,
                             triggertime=TRIGGERTIME, triggername=TRIGGERNAME,
                             outdir=DATA_PATH)
-
+    
     PIPELINE.get_coverage(in_map=LIGO_MAP, ft2=ft2, start_time=TRIGGERTIME,
                             stop_time=TRIGGERTIME+10000., theta_cut=THETAMAX,
                             zenith_cut=ZMAX, outroot=OUTCOV)
-
+        
+    PIPELINE.adaptive_time(in_map=LIGO_MAP, ft2=ft2, triggertime=TRIGGERTIME,
+                            theta_cut=THETAMAX, zenith_cut=ZMAX, nside=32,
+                            roi=0, output=OUTTIME)
+    
     PIPELINE.prepare_grid(map=LIGO_MAP, out_list=OUTLIST, out_map=OUTMAP)
 
 if __name__ == '__main__':
