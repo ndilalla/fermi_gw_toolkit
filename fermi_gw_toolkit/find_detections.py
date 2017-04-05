4cdca8b87f5595596f68b8657e72339bdcb9ba20
giacomov <giacomo.vianello@gmail.com>
2017-04-05 08:47:37 -0700

    Final touches

--- fermi_gw_toolkit/find_detections.py
+++ fermi_gw_toolkit/find_detections.py
@@ -8,20 +8,21 @@ import healpy as hp
 import scipy.stats
 
 from fermi_gw_toolkit.check_file_exists import check_file_exists
-from fermi_gw_toolkit.contour_finder import pix_to_sky
+from fermi_gw_toolkit.contour_finder import pix_to_sky, ContourFinder
 
-def Hopkins_method(healpix_tsmap, fdr, psf_size, hopkins_correction=True):
 
-    ts_map_array, header = hp.read_map(healpix_tsmap, h=True, verbose=False)
-
-    N = ts_map_array.shape[0]
+def Hopkins_method(ts_map_array, ra, dec, fdr, psf_size, N, nside, hopkins_correction=True):
 
     # Compute the number of correlated pixels, i.e., the number of pixels within a PSF distance
-    nside = hp.get_nside(ts_map_array)
+    # NOTE: query_disc gets the *radius* of the disc as input, not the diameter, so we can directly use psf_size
     n_correlated_pixels = len(hp.query_disc(nside, (0, 0, 1), np.deg2rad(psf_size), inclusive=True, fact=4))
 
     print("Got a map with nside %i. Using a number of correlated pixels of %i" % (nside, n_correlated_pixels))
-    print("Correlation radius: %.2f deg" % (np.sqrt(n_correlated_pixels / np.pi) * np.rad2deg(hp.max_pixrad(nside))))
+
+    # This number should be close to psf_size (within ~1 deg)
+    approx_corr_rad = np.sqrt(n_correlated_pixels / np.pi) * np.rad2deg(hp.max_pixrad(nside))
+
+    print("Approx. correlation radius (should be within 1 deg of PSF size): %.2f deg" % approx_corr_rad)
 
     # This notation follows Clements, Sarkar and Guo (2012):
     # https://web.njit.edu/~wguo/Clements_Sarkar_Guo%202012.pdf
@@ -67,10 +68,6 @@ def Hopkins_method(healpix_tsmap, fdr, psf_size, hopkins_correction=True):
 
         # Detections
 
-        # Compute coordinates of points
-
-        ra, dec = pix_to_sky(np.arange(N), hp.get_nside(ts_map_array))
-
         # Find out the last point above the threshold, all points before in the sorted p_values array
         # are considered detection
 
@@ -92,8 +89,13 @@ if __name__ == "__main__":
     formatter = argparse.ArgumentDefaultsHelpFormatter
     parser = argparse.ArgumentParser(description=desc, formatter_class=formatter)
 
-    parser.add_argument('--tsmap', help='Input HEALPIX TS map',
+    parser.add_argument('--tsmap', help='Input TS map',
                         type=check_file_exists, required=True)
+    parser.add_argument('--ligomap', help='Input LIGO map', type=check_file_exists,
+                        required=True)
+    parser.add_argument('--cl',
+                        help='Containment level for the contour (default: 0.9, i.e., 90 percent)', default=0.9,
+                        type=float)
     parser.add_argument('--false_detection_rate', help='FDR for the search',
                         type=float, required=False, default=0.01)
     parser.add_argument('--psf_size', help='Radius of the PSF to be used as correlation region in the Hopkins '
@@ -104,7 +106,26 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    ra, dec, TS = Hopkins_method(args.tsmap, args.false_detection_rate, args.psf_size, args.use_hopkins)
+    # Get NSIDE from the TS map
+    ts_map_array, header = hp.read_map(args.tsmap, h=True, verbose=False)
+
+    # Get NSIDE
+    nside = hp.get_nside(ts_map_array)
+
+    # Compute the contours and get the points within the contour
+    my_finder = ContourFinder(args.ligomap, nside)
+
+    indexes = my_finder.find_contour(args.cl)
+    ra, dec = my_finder.get_sky_coordinates(indexes)
+
+    N = indexes.shape[0]
+
+    print("Found %s points within the %s percent containment level" % (N, args.cl * 100.0))
+
+    # Apply the FDR threshold
+
+    ra, dec, TS = Hopkins_method(ts_map_array[indexes], ra, dec, args.false_detection_rate,
+                                 args.psf_size, N, nside, args.use_hopkins)
 
     print("Found %i detection(s)" % len(ra))
 
