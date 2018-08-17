e5e79f5862f05789159dc1cfb384c96639424dc7
Nicola <nicola.omodei@gmail.com>
2018-08-17 15:00:04 -0700

    Added the possibility to chnage the CL

--- fermi_gw_toolkit/slac/prepare_grid.sh
+++ fermi_gw_toolkit/slac/prepare_grid.sh
@@ -4,5 +4,5 @@ echo 'sourcing the setup script!'
 source $GPL_TASKROOT/config/DEV/setup_gw_giacomvst.csh
 rm -rf ${OUTPUT_FILE_PATH}/FIXEDINTERVAL/*
 echo 'About run the prepare_grid.py script...'
-echo python $GPL_TASKROOT/fermi_gw_toolkit/fermi_gw_toolkit/prepare_grid.py --map $HEALPIX_PATH_MAP --out_list $OUTLIST --out_map $OUTMAP --nside $NSIDE
-python $GPL_TASKROOT/fermi_gw_toolkit/fermi_gw_toolkit/prepare_grid.py --map $HEALPIX_PATH_MAP --out_list $OUTLIST --out_map $OUTMAP --nside $NSIDE
+echo python $GPL_TASKROOT/fermi_gw_toolkit/fermi_gw_toolkit/prepare_grid.py --map $HEALPIX_PATH_MAP --out_list $OUTLIST --out_map $OUTMAP --nside $NSIDE --cl $LIGO_COVERAGE_CL
+python $GPL_TASKROOT/fermi_gw_toolkit/fermi_gw_toolkit/prepare_grid.py --map $HEALPIX_PATH_MAP --out_list $OUTLIST --out_map $OUTMAP --nside $NSIDE --cl $LIGO_COVERAGE_CL 
