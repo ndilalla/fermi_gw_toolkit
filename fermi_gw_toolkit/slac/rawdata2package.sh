e07031f754e2b8b52e973e875dd41f25dd64d76a
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-08-17 16:08:00 -0700

    Pointing at the rawdata2package.py executable in the PATH

--- fermi_gw_toolkit/slac/rawdata2package.sh
+++ fermi_gw_toolkit/slac/rawdata2package.sh
@@ -5,6 +5,6 @@ echo 'sourcing the setup script!'
 source $GPL_TASKROOT/config/DEV/setup_gw_giacomvst.csh
 echo 'About to run the rawdata2package.py script...'
 echo rawdata2package.py --ft1 $FT1_PATH --ft2 $FT2_PATH --triggername $TRIGGERNAME --triggertime $TRIGGERTIME --outdir $OUTPUT_FILE_PATH/$TRIGGERNAME
-python rawdata2package.py --ft1 $FT1_PATH --ft2 $FT2_PATH --triggername $TRIGGERNAME --triggertime $TRIGGERTIME --outdir $OUTPUT_FILE_PATH/$TRIGGERNAME
+rawdata2package.py --ft1 $FT1_PATH --ft2 $FT2_PATH --triggername $TRIGGERNAME --triggertime $TRIGGERTIME --outdir $OUTPUT_FILE_PATH/$TRIGGERNAME
 chmod -R 777 $OUTPUT_FILE_PATH
 
