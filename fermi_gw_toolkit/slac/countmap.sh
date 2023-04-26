4aa5d8a5b8c9c533fa67efa7600cd32843f3c99e
ndilalla <niccolo.dilalla@stanford.edu>
2023-04-25 17:58:41 -0700

    Adding verbose flag.

--- fermi_gw_toolkit/slac/countmap.sh
+++ fermi_gw_toolkit/slac/countmap.sh
@@ -1,4 +1,4 @@
-#!/bin/tcsh -vf
+#!/bin/tcsh -f
 # If we do not need to run the FTI analysis, make this fail
 if ($RUN_PGW == 0) then
 exit 1
@@ -7,6 +7,8 @@ endif
 # THIS SHOULD SOURCE THE APPROPRIATE FILES TO SET UP THE ENVIRONMENT AND EXCECUTE THE PYTHON SCRIPT
 echo 'sourcing the setup script!'
 source $GPL_TASKROOT/set_env/setup_gwfup.csh
+
+set echo
 
 echo 'running gtselect...'
 gtselect infile=${FT1_PATH} outfile=${PGW_FT1} ra=180 dec=0 rad=180 tmin=${MET_TSTART} tmax=${MET_TSTOP} emin=${EMIN} emax=${EMAX} zmax=${ZMAX}
--- fermi_gw_toolkit/slac/rawdata2package.sh
+++ fermi_gw_toolkit/slac/rawdata2package.sh
@@ -1,15 +1,17 @@
-#!/bin/bash -ve
+#!/bin/bash -e
 # THIS SHOULD SOURCE THE APPROPRIATE FILES TO SET UP THE ENVIRONMENT AND EXCECUTE THE PYTHON SCRIPT
 
 echo 'Sourcing the setup script-->'
 source $GPL_TASKROOT/set_env/setup_gwfup.sh
-echo 'About to run the rawdata2package.py script...'
+
+set -x
 
 mkdir -p $OUTPUT_FILE_PATH/$TRIGGERNAME
 
 echo "Creating and moving to $OUTPUT_FILE_PATH/$TRIGGERNAME"
 cd $OUTPUT_FILE_PATH/$TRIGGERNAME
 
+echo 'About to run the rawdata2package.py script...'
 python $GTBURST_PATH/scripts/rawdata2package.py $FT1_PATH $FT2_PATH $TRIGGERTIME $TRIGGERNAME 0.0 0.0
 
 chmod -R 777 $OUTPUT_FILE_PATH
