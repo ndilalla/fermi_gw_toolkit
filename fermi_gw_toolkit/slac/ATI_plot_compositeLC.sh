15e64a63f050a2d2cf099df4b79f11e6a9dd2341
ndilalla <niccolo.dilalla@stanford.edu>
2023-04-25 21:31:02 -0700

    More bug fixes.

--- fermi_gw_toolkit/slac/ATI_plot_compositeLC.sh
+++ fermi_gw_toolkit/slac/ATI_plot_compositeLC.sh
@@ -5,7 +5,7 @@ source $GPL_TASKROOT/set_env/setup_gwfup.csh
 
 set echo
 
-echo 'About to run plot_lighcturve.py'
-python ${FERMI_GWTOOLS}/bin/plot_lighcturve.py --input ${OUTPUT_FILE_PATH}/ADAPTIVEINTERVAL/${TRIGGERNAME}_res_all.txt --triggertime $TRIGGERTIME --out_plot ${ATI_COMPOSITELC_PLOT} --nside $NSIDE --xlabel $TRIGGERNAME --histo 1 --type EFLUX
+echo 'About to run plot_lightcurve.py'
+python ${FERMI_GWTOOLS}/bin/plot_lightcurve.py --input ${OUTPUT_FILE_PATH}/ADAPTIVEINTERVAL/${TRIGGERNAME}_res_all.txt --triggertime $TRIGGERTIME --out_plot ${ATI_COMPOSITELC_PLOT} --nside $NSIDE --xlabel $TRIGGERNAME --histo 1 --type EFLUX
 
 chmod a+w ${ATI_COMPOSITELC_PLOT}
