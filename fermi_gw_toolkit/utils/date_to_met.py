332d752eb937399ae1163d820c8caa8e32977c85
ndilalla <niccolo.dilalla@stanford.edu>
2023-05-03 18:46:41 -0700

    Added met_to_utc function.

--- fermi_gw_toolkit/utils/date_to_met.py
+++ fermi_gw_toolkit/utils/date_to_met.py
@@ -1,5 +1,6 @@
 import astropy.io.fits as pyfits
-from GtBurst.dataHandling import date2met
+from astropy.time import Time
+from GtBurst.dataHandling import date2met, met2date
 
 def get_met(ligo_map):
     hdulist = pyfits.open(ligo_map)
@@ -7,4 +8,6 @@ def get_met(ligo_map):
     date_obs = '%s %s' %(date_obs[:10], date_obs[11:])
     return date2met(date_obs)
 
-
+def met_to_utc(met):
+    date = met2date(met)
+    return Time(date, scale='utc')
