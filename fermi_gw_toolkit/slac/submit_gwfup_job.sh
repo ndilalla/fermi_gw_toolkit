bc693fd151aa4b21c2362590177be9aecc6238f6
ndilalla <niccolo.dilalla@stanford.edu>
2023-05-31 16:24:03 -0700

    Adding nside to command line arguments.

--- fermi_gw_toolkit/slac/submit_gwfup_job.sh
+++ fermi_gw_toolkit/slac/submit_gwfup_job.sh
@@ -4,9 +4,9 @@ cd /nfs/farm/g/glast/u26/GWFUP/
 source set_env/setup_gwfup.sh
 
 echo "Current date and time: $(date)"
-echo "About to submit a new GWFUP job using the sky map at $1"
+echo "About to submit a new GWFUP job using the sky map available at $1 and NSIDE $2"
 
 set -x
-python fermi_gw_toolkit/fermi_gw_toolkit/tools/submit_gwfup_job.py --url $1 --nside 64 --version v01 --run_bayul 1 --pixels_job 5 --wall_time 4
+python fermi_gw_toolkit/fermi_gw_toolkit/tools/submit_gwfup_job.py --file $1 --nside $2 --version v01 --run_bayul 1 --pixels_job 5 --wall_time 4
 
 cd -
