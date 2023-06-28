19aff12f3d74a539419df315c2d45eb38aa78f0f
ndilalla <niccolo.dilalla@stanford.edu>
2023-06-28 11:35:10 -0700

    More work.

--- fermi_gw_toolkit/tools/cleanup_npz.py
+++ fermi_gw_toolkit/tools/cleanup_npz.py
@@ -5,10 +5,14 @@ from fermi_gw_toolkit import GPL_TASKROOT
 
 output_dir = os.path.join(GPL_TASKROOT, 'output')
 fti_dirs = sorted(glob('%s/*/*/FIXEDINTERVAL/' % (output_dir)))
+print('Found %d folders.' % len(fti_dirs))
+print('Processing the first %d' % len(fti_dirs[:-10]))
 
-for fti_dir in fti_dirs:
+for fti_dir in fti_dirs[:-10]:
     npz_files = glob('%s/*_bayesian_ul*.npz' % fti_dir)
-    print(npz_files)
-
-
-
+    for npz_file in npz_files:
+        cmd = 'rm %s' % npz_file
+        #os.system(cmd)
+        input()
+    print('%d files deleted from %s' % (len(npz_files), fti_dir))
+    
