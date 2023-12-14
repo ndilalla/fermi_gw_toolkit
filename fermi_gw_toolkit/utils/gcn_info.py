04b8cc67b07fbcb96d80b079f8055d5d0c8dd572
ndilalla <niccolo.dilalla@stanford.edu>
2023-12-14 14:42:42 -0800

    Switched back to use s3df

--- fermi_gw_toolkit/utils/gcn_info.py
+++ fermi_gw_toolkit/utils/gcn_info.py
@@ -26,7 +26,7 @@ def check_url(url):
     except RuntimeError:
         return False
 
-def curl_s3df(url, outfile=None, shell=True, text=False, galprop=True):
+def curl_s3df(url, outfile=None, shell=True, text=False, galprop=False):
     #ssh ndilalla@s3dflogin.slac.stanford.edu curl -S https://gracedb.ligo.org/api/superevents/S200316bj/files/bayestar.fits.gz -o - > prova.fits
     #ssh ndilalla@galprop.stanford.edu "source source_gwtable.sh && curl -S https://gracedb.ligo.org/api/superevents/S200316bj/files/bayestar.fits.gz -o -" > prova.fits
     cmd = f'ssh ndilalla@s3dflogin.slac.stanford.edu curl -S {url} -o -'
