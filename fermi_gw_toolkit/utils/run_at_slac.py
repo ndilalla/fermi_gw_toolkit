1edc5b2a558e252ac250cfdf8ab0719a75ca463b
ndilalla <niccolo.dilalla@stanford.edu>
2025-01-07 20:17:29 +0100

    Minor.

--- fermi_gw_toolkit/utils/run_at_slac.py
+++ fermi_gw_toolkit/utils/run_at_slac.py
@@ -4,6 +4,7 @@ def run_at_slac():
     """Decide if we are running locally at SLAC or not
     """
     hostname = socket.getfqdn()
+    print("Running on host: ", hostname)
     run_at_slac = True
     if hostname.find("slac.stanford.edu") == -1:
         run_at_slac = False
